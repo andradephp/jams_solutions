7110a76462ac9a3851e78497160155032be2dd47
andradephp <steven.andrade.stat@gmail.com>
2025-06-02 00:48:07 -0500

    "Agregar configuacion proyecto + base de datos"

---
+++ app/Views/login/loginConex.php
@@ -0,0 +1,35 @@
+<?php 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+   <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+   <?php foreach($conexion as $users):?>
+        <tr>
+            <td><?php echo $users -> id_usuario ;?></td>
+            <td><?php echo $users -> nombre_usuario ;?></td>
+            <td><?php echo $users -> apellido_usuario ;?></td>
+            <td><?php echo $users -> telefono_usuario ;?></td>
+            <td><?php echo $users -> email_usuario ;?></td>
+        </tr>
+        <?php endforeach; ?>
+  </tbody>
+</table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
